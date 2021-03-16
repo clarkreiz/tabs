5f1aa87ebf26430cb0ac5aa9e52d7f6ff911e4f0
akhabarov <akhabarov@freetechs.org>
2021-03-16 07:53:35 +0300

    first step

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const buttonsContainer = document.querySelector('.tabs__list');
+buttonsContainer.addEventListener('click', event => {
+	let index = event.target.id;
+   
+    if (event.target.id) {
+        document.querySelector('.tabs__section--active').classList.remove('tabs__section--active');
+        document.querySelector(`#tab${index}`).classList.add('tabs__section--active');
+        console.log(document.querySelector(`#tab${index}`));
+    }
+});
