aaefa1fd9bcf1fa510ad444af64466d9e2fb94af
akhabarov <akhabarov@freetechs.org>
2021-03-19 11:20:10 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const tabsList = document.querySelector('.tabs__list');
-tabsList.addEventListener('click', event => {
-	let index = event.target.id;
-   
+
+const selectTab = event => {
+    let index = event.target.id;
     if (event.target.id) {
         document.querySelector('.tabs__section--active').classList.remove('tabs__section--active');
         document.querySelector(`#tab${index}`).classList.add('tabs__section--active');
-        console.log(document.querySelector(`#tab${index}`));
     }
-});
+}
+tabsList.addEventListener('click', selectTab);
