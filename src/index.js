19df0297d8d7f51be5baf708febe7df33e11251b
akhabarov <akhabarov@freetechs.org>
2021-03-19 11:20:51 +0300

    syntax fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const tabsList = document.querySelector('.tabs__list');
 
-const selectTab = event => {
+const selectTab = (event) => {
     let index = event.target.id;
     if (event.target.id) {
         document.querySelector('.tabs__section--active').classList.remove('tabs__section--active');
